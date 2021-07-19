ff9a7c6280e54c6f7018e0b6d28790cb1bf7d4f7
Pedro Gusmao <pedro.gusmao1998@hotmail.com>
2021-07-19 14:17:05 -0300

    conversor

---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ConversorComponent } from './components';
+import { MoedaService, ConversorService } from './services';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    ConversorComponent
+    
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  exports: [
+    ConversorComponent
+  ],
+  providers: [
+    MoedaService,
+    ConversorService
+  ]
+})
+export class ConversorModule { }
